14b2f6f1305ce603ddfb538bbbcd61de45c4fc58
richardturnerdeveloper <richardturnerdeveloper@gmail.com>
2018-01-02 21:41:25 -0500

    Add fixtures file for testing.

---
+++ src/tests/fixtures/meals.js
@@ -0,0 +1,13 @@
+export default [{
+    name: 'Peanut Butter',
+    calories: 190,
+    protein: 7,
+    carbs: 12,
+    fat: 10
+}, {
+    name: 'Cottage Cheese',
+    calories: 80,
+    protein: 2,
+    carbs: 3,
+    fat: 7
+}];
